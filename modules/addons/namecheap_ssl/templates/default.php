b89495d5860be4f60c8f27e9d61b69a24a327200
namecheapnithya <nithya@logicgenie.com>
2015-11-23 21:33:32 +0530

    Update default.php

--- modules/addons/namecheap_ssl/templates/default.php
+++ modules/addons/namecheap_ssl/templates/default.php
@@ -2,4 +2,5 @@
     <li><a href="<?php echo $view['global']['mod_url']?>&action=log"><?php echo $_LANG['ncssl_addon_menu_log']?></a></li>
     <li><a href="<?php echo $view['global']['mod_url']?>&action=sync"><?php echo $_LANG['ncssl_addon_menu_sync']?></a></li>
     <li><a href="<?php echo $view['global']['mod_url']?>&action=list"><?php echo $_LANG['ncssl_addon_menu_list']?></a></li>
+    <li><a href="<?php echo $view['global']['mod_url']?>&action=settings"><?php echo $_LANG['ncssl_addon_menu_settings']?></a></li>
 </ul>
